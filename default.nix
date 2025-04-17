1c5ef454f4628c71e89b54a75aab532bdc2d150e
Gongqi Huang <gongqih@hotmail.com>
2025-04-17 14:57:48 -0400

    nix: fix minor

--- default.nix
+++ default.nix
@@ -25,10 +25,9 @@ maven.buildMavenPackage {
   src = lib.cleanSourceWith {
     filter = (path: _type: (builtins.baseNameOf (builtins.toString path)) != "xtrace-data");
     src = ./.;
-  }; 
+  };
 
   mvnHash = "sha256-AuqoNezjLg3JCQodD6PlxMQ5F0USEtyBsTHxCJ7+ZcA=";
-  # mvnHash = "sha256-Co1gcUaN3MTVLCam9h5aauyXrp3LJ6q+q6nFDbIU2fk=";
 
   mvnParameters = lib.escapeShellArgs [
     "clean"
